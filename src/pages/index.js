f8f7bbbf5adcad6597d0f63d5f8cee1b7afdd7bd
AJ Srivastava <aj.srivastava@monsanto.com>
2018-06-21 18:19:07 -0500

    initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+    <div className='container-fluid'>
+        <div className='row'>
+            <div className='col-12'>
+                <h1>1904labs Knowledge Base</h1>
+            </div>
+        </div>
+    </div>
+)
+
+export default IndexPage
